cbddd1ddc5e1e11b43f2a1a6ab379ae12ce005ce
3A932004 <s3a932004@student.ncut.edu.tw>
2023-01-02 12:29:46 +0800

    修正首頁跳轉登入、修正主頁顯示加入訂單按鈕

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,19 +10,21 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $rooms = Room::all();
+        $data = [
+            'rooms' => $rooms
+        ];
         if(Auth::check()){//已登入
             /*if(Auth::user()->ismember == '0'){
                 return redirect('/rooms');
             }else if(Auth::user()->ismember == '1'){
                 return redirect('/');
             }*/
-            $rooms = Room::all();
-            $data = [
-                'rooms' => $rooms
-            ];
+
             return view('home.index', $data);
         }else{
-            return redirect('/login');
+//            return redirect('/login');
+            return view('home.index', $data);
         }
 
     }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,7 +34,7 @@
                      <div class="card bg-light border-0 h-100">
                          <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                              <h2 class="fs-4 fw-bold">房號：{{$room->id}}</h2>
-                            <p class="mb-0">{{ $room->introduce }}</p><br>
+                            <p class="mb-0">{{ Str::limit($room->introduce, 80) }}</p><br>
                             <p class="mb-0">可住人數：{{ $room->people }}</p>
                             <p class="mb-0">金額：${{ $room->amount }}</p>
                             <p class="mb-0">目前狀態：
@@ -50,8 +50,9 @@
                             </p>
 {{--                            <p class="mb-0">目前狀態：{{($room->shelf_status)? '開放訂購' : '整理中'}}</p>--}}
 {{--                            <a class="btn btn-success btn-sm" href="{{ route('orders.show') }}">詳細資訊</a>--}}
-
-                            <a class="btn btn-success btn-sm" href="orders/create/{{$room->id}}">加入訂單</a>
+                            @if($room->shelf_status==1)
+                                <a class="btn btn-success btn-sm" href="orders/create/{{$room->id}}">加入訂單</a>
+                            @endif
                         </div>
                     </div>
                 </div>
